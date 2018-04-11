ffbd6ce98fa1df228ef86c3c2770d093fdc9c262
Carl Claesson <cfclaesson@gmail.com>
2018-04-11 20:59:26 +0200

    Update comments

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
 fetch('https://api.stormglass.io/forecast?lat=58.5&lng=17.8', {
-    headers: {
-      'Authentication-Token': 'YOUR API KEY'  // Get your key by emailing account@stormglass.io
-    }
-  }).then(function(response) {
-    // Do something with response data.
-    var jsonData = response.json();
-  });
+  headers: {
+    'Authentication-Token': 'YOUR API KEY'  // Get your at https://stormglass.io
+  }
+}).then(function(response) {
+  // Do something with response data.
+  var jsonData = response.json();
+});
